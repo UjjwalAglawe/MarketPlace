70e90030769640823012ba1bcfb74b23f46a427f
UjjwalAglawe <127344565+UjjwalAglawe@users.noreply.github.com>
2024-02-29 22:17:49 +0530

    second commit

--- marketplace/src/redux/Slices/CartSlice.js
+++ marketplace/src/redux/Slices/CartSlice.js
@@ -6,10 +6,10 @@ export const CartSlice=createSlice({
     initialState:[],
     reducers:{
         add:(state,action)=>{
-            state.push(action.payload) //jo bhi input parameter store kiya hai use action.payload se access kar sakte hai ....hum use state ke andar push karre hai  ..... here in Product 
+            state.push(action.payload) 
         },
         remove:(state,action)=>{
-            return state.filter((item)=>item.id!==action.payload)  //state ke andar vo hi value store karna jiski id != action .payload
+            return state.filter((item)=>item.id!==action.payload)  
         },
     }
 })
